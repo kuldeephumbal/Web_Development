4f8abde12a2697bdc692bea00b8f862de795e8fe
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-03-05 16:15:50 +0530

    Add contact form component

---
+++ REACTJS/app1/src/05_form.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+var page = (<div>
+    <div className="contanier-fluid">
+        <div className="row">
+            <div className="col-md-6">
+                <form>
+                    <h1>Contact Form</h1>
+                    <div className="form-group">
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" className="form-control" id="name" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Message:</label>
+                        <textarea className="form-control" id="message" rows="3"></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+);
+root.render(page);
+
+
