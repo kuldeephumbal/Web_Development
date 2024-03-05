8172856b37f16d68402faaaa5652b66f1e4c263b
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-03-05 16:15:00 +0530

    Add hello world component to app1

---
+++ REACTJS/app1/src/01_hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+var output = <h1 align="center">Hello World!</h1>
+root.render(output);
+
+
