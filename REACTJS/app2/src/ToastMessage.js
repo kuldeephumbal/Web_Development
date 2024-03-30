70aba9b23a87624e8ce7c764b15d3d5d7b45d1b0
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-03-30 17:04:10 +0530

    Toast message component

---
+++ REACTJS/app2/src/ToastMessage.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import {toast, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const DefaultConfig =  {
+    position: "bottom-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    transition: Bounce,
+    }
+export function NetworkError(error = null)
+{
+    if(error === null)
+        showError("Either you are or server is offline");
+    else 
+    {
+        axios({
+            url:'http://www.amazon.in',
+            method:'get',
+            responseType:'text'
+        }).then((response) => {
+            showError('server is down, please contact administrator')
+        }).catch((error) => {
+            console.log(error.code);
+            if(error.code === 200)
+                showError('server is down, please contact administrator')
+            else
+                showError('you are offline, check your internet connection');
+        })
+    }
+}
+export function showError(message)
+{
+    toast.error(message, DefaultConfig);
+}
+export function showMessage(message)
+{
+    toast.success(message, DefaultConfig);
+}
