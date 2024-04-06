8d18dd150ea136109b647f0ab1f2e7ccf62b4396
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-04-06 12:04:25 +0530

    Add VerifyLogin component for authentication

---
+++ REACTJS/app2/src/Verifylogin.js
@@ -0,0 +1,14 @@
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+
+export default function VerifyLogin() {
+    let [cookies, setCookie, removeCookie] = useCookies(['theeasylearn']);
+    let navigate = useNavigate();
+
+    console.log(cookies['adminid']);
+
+    if (cookies['adminid'] === undefined && cookies['doctorid'] === undefined) 
+    {
+        navigate("/");
+    }
+}
