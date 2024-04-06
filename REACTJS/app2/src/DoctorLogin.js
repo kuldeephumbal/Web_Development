a98bdabcd08061d5c8db5abaf6432ce47e90bda8
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-04-06 12:20:04 +0530

    Refactor DoctorLogin component to use React hooks and add login functionality

--- REACTJS/app2/src/DoctorLogin.js
+++ REACTJS/app2/src/DoctorLogin.js
@@ -1,8 +1,64 @@
-export default function DoctorLogin()
-{
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { NetworkError, showError, showMessage } from "./ToastMessage";
+import getBase from "./Api";
+import { ToastContainer } from "react-toastify";
+import { useCookies } from 'react-cookie';
+
+export default function DoctorLogin() {
+  let [cookies, setCookie, removeCookie] = useCookies(['theeasylearn']);
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let navigate = useNavigate();
+
+  let doctorLogin = function(event) {
+    event.preventDefault();
+    console.log(email, password);
+
+    let apiAdrress = getBase() + "doctor_login.php";
+    console.log(apiAdrress);
+
+    let form = new FormData();
+    form.append("email", email);
+    form.append("password", password);
+
+    axios({
+      method: 'post',
+      responseType:'json',
+      url: apiAdrress,
+      data: form
+    }).then((response) => {
+      console.log(response.data);
+      let error = response.data[0]['error'];
+      if (error !== 'no'){
+        showError(error);
+      }
+      else {
+        let success = response.data[1]['success'];
+        let message = response.data[2]['message'];
+        if (success !== 'yes'){
+          showError(message);
+        }
+        else {
+          showMessage(message);
+          let doctorid = response.data[3]['id'];
+          setCookie("doctorid",doctorid);
+          console.log("doctor id =",cookies['doctorid']);
+          setTimeout(() => {
+            navigate("/admin-appointments/" + doctorid);
+          },2000);
+        }
+      }
+    }).catch((error) => {
+      NetworkError(error);  
+    });
+  }
     return(<main>
         <div className="container">
           <section className="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+            <ToastContainer />
             <div className="container">
               <div className="row justify-content-center">
                 <div className="col-lg-5 col-12 d-flex flex-column align-items-center justify-content-center">
@@ -18,18 +74,20 @@ export default function DoctorLogin()
                       <div className="pt-4 pb-2">
                         <h5 className="card-title text-center pb-0 fs-4">Doctor Login</h5>
                       </div>
-                      <form className="row g-3 needs-validation" noValidate>
+                      <form className="row g-3 needs-validation" onSubmit={doctorLogin}>
                         <div className="col-12">
                           <label htmlFor="youremail" className="form-label">Email</label>
                           <div className="input-group has-validation">
                             <span className="input-group-text" id="inputGroupPrepend">@</span>
-                            <input type="email" name="email" className="form-control" id="youremail" required />
+                            <input type="email" name="email" className="form-control" id="youremail" 
+                            value={email} onChange={(event) => setEmail(event.target.value)} required />
                             <div className="invalid-feedback">Please enter your username.</div>
                           </div>
                         </div>
                         <div className="col-12">
                           <label htmlFor="yourPassword" className="form-label">Password</label>
-                          <input type="password" name="password" className="form-control" id="yourPassword" required />
+                          <input type="password" name="password" className="form-control" id="yourPassword" 
+                          value={password} onChange={(event) => setPassword(event.target.value)} required />
                           <div className="invalid-feedback">Please enter your password!</div>
                           <div className="mt-1">
                             <a href="doctor-forgot.html">Forgot password?</a>
@@ -39,7 +97,7 @@ export default function DoctorLogin()
                           <button className="btn btn-primary w-100" type="submit">Login</button>
                         </div>
                         <div className="col-12">
-                          <p className="small mb-0">Don't have account? <a href="pages-register.html">Create an account</a></p>
+                          <p className="small mb-0">Don't have account? <Link to='/doctor-login'>Create an account</Link></p>
                         </div>
                       </form>
                     </div>
