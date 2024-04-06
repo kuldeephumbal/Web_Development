96812b9dc2a78cfbdebb8d36d7063eccd1402fc2
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-04-06 12:18:35 +0530

    Update index.js with new Greeting component

--- REACTJS/app1/src/index.js
+++ REACTJS/app1/src/index.js
@@ -4,33 +4,9 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-var page = (<div>
-    <div className="contanier-fluid">
-        <div className="row">
-            <div className="col-md-6">
-                <form>
-                    <h1>Contact Form</h1>
-                    <div className="form-group">
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" className="form-control" id="name" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" className="form-control" id="email" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="message">Message:</label>
-                        <textarea className="form-control" id="message" rows="3"></textarea>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
-            </div>
-        </div>
-    </div>
-
-
-</div>
-);
-root.render(page);
-
+let item = {name: 'kuldeep ', sername: 'humbal'};
+function Greeting(item){
+    return <h1>Hello {item.name}{item.sername}</h1>
+}
+root.render(Greeting(item));
 
