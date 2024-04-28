d19bcfdf9f63bfc64c999cb3e4ac6be79214deca
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-04-28 18:37:27 +0530

    Add NoPageFound component for displaying a 404 error page in NoPageFound.js

---
+++ REACTJS/app3/src/NoPageFound.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Menu from './Menu';
+import Footer from './Footer';
+export default class Page extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<>
+            <div className="error-area overlay">
+                <div className="d-table">
+                    <div className="d-table-cell">
+                        <div className="container">
+                            <div className="error-content">
+                                <h1>404</h1>
+                                <h2>Oops! Page Not Found!</h2>
+                                <p>The page you are looking for does not exist. It might have been moved or deleted.</p>
+                                <div className="button">
+                                    <a href="index.html" className="btn">Back to Home</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+        );
+    }
+}
