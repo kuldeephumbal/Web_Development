324444328ce3246ac9ed0b4a6e629959827d08a6
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-05-10 13:11:09 +0530

    chore: Remove commented out code in main.js

--- REACTJS/app3/public/site/assets/js/main.js
+++ REACTJS/app3/public/site/assets/js/main.js
@@ -28,6 +28,17 @@ Author: GrayGrids
         } else {
             header_navbar.classList.remove("sticky");
         }
+
+
+
+
+        // show or hide the back-top-top button
+        // var backToTo = document.querySelector(".scroll-top");
+        // if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        //     backToTo.style.display = "flex";
+        // } else {
+        //     backToTo.style.display = "none";
+        // }
     };
 
 
