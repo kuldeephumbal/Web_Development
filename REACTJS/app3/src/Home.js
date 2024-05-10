59226f9f7e2ebe672eb28399c7986c10238c3733
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-05-10 13:12:03 +0530

    refactor: Update Doctor component to use axios for API calls and refactor code structure

--- REACTJS/app3/src/Home.js
+++ REACTJS/app3/src/Home.js
@@ -1,26 +1,134 @@
 import React from 'react';
 import Menu from './Menu';
 import Footer from './Footer';
+import axios from 'axios';
+import getBase from './Api';
+import { showError, showMessage } from './ToastMessage'
+import { ToastContainer } from "react-toastify";
+import { Link } from "react-router-dom";
+import { withCookies } from 'react-cookie';
 
-let SpecalitiesItem = (props) => {
-    return (<div className="col-lg-4 col-md-12 col-12">
-        <div className="portfolio-sidebar">
-            <div className="single-widget researcher-details">
-                <h2 className="text-center text-success">Dental Care Treatment</h2>
-            </div>
-
-        </div>
-    </div>)
-}
-
-export default class Home extends React.Component {
+class Home extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            city: [],
+            department: [],
+            services: [],
+            selectedCity: '',
+            selectedDepartment: '',
+            isDataFetched: false
+        }
+    }
+    GetCity = () => {
+        let apiAddress = getBase() + "city.php";
+        axios({
+            method: 'get',
+            url: apiAddress,
+            responseType: 'json'
+        }).then((response) => {
+            console.log(response);
+            let error = response.data[0]['error'];
+            if (error !== 'no') {
+                showError(error);
+            }
+            else {
+                let total = response.data[1]['total'];
+                if (total === 0)
+                    showError('no city found')
+                else {
+                    response.data.splice(0, 2);
+                    this.setState({
+                        city: response.data
+                    });
+                }
+            }
+        }).catch((error) => {
+            showError('could not connect to server');
+        });
+    }
+    GetDepartmentByCity = (SelectedCity) => {
+        this.setState({
+            selectedCity: SelectedCity
+        });
+        let apiAddress = getBase() + "get_service_by_city.php?city=" + SelectedCity;
+        axios({
+            method: 'get',
+            url: apiAddress,
+            responseType: 'json'
+        }).then((response) => {
+            console.log(response);
+            let error = response.data[0]['error'];
+            if (error !== 'no') {
+                showError(error);
+            }
+            else {
+                let total = response.data[1]['total'];
+                if (total === 0)
+                    showError('no service found');
+                else {
+                    response.data.splice(0, 2);
+                    this.setState({
+                        department: response.data
+                    });
+                }
+            }
+        }).catch((error) => {
+            showError('could not connect to server');
+        });
+    }
+    GetDepartment = () => {
+        let apiAddress = getBase() + "get_unique_service.php";
+        axios({
+            method: 'get',
+            url: apiAddress,
+            responseType: 'json'
+        }).then((response) => {
+            console.log(response);
+            let error = response.data[0]['error'];
+            if (error !== 'no') {
+                showError(error);
+            }
+            else {
+                let total = response.data[1]['total'];
+                if (total === 0)
+                    showError('no services found')
+                else {
+                    response.data.splice(0, 2);
+                    this.setState({
+                        services: response.data,
+                        isDataFetched: true
+                    });
+                }
+            }
+        }).catch((error) => {
+            showError('could not connect to server');
+        })
+    }
+    componentDidMount() {
+        if (this.state.isDataFetched === false) {
+            this.GetCity();
+            this.GetDepartment();
+        }
+    }
+    onchangeService = (SelectedService) => {
+        this.setState({
+            selectedService: SelectedService
+        });
+    }
+    submitted = (e) => {
+        e.preventDefault();
+        console.log(this.state.selectedCity, this.state.selectedService);
+        const { cookies } = this.props; 
+        cookies.set('city', this.state.selectedCity, { path: '/' });
+        cookies.set('serviceid', this.state.selectedService, { path: '/' });
+        window.location = "/doctor";
     }
     render() {
         return (<>
             <Menu />
             {/* Start Hero Area */}
+            <ToastContainer />
             <section className="hero-area style2">
                 <div className="hero-slider">
                     {/* Start Single Slider */}
@@ -36,8 +144,8 @@ export default class Home extends React.Component {
                                             <p className="wow fadeInLeft" data-wow-delay=".5s">Our teaming has been focused on
                                                 building a high-qualities medicals service by MediAppoint.</p>
                                             <div className="button wow fadeInLeft" data-wow-delay=".7s">
-                                                <a href="appointment.html" className="btn">Book Appointment</a>
-                                                <a href="about-us.html" className="btn alt">About Us</a>
+                                                <Link to="/booking-aapointment" className="btn">Book Appointment</Link>
+                                                <Link to="#" className="btn alt">About Us</Link>
                                             </div>
                                         </div>
                                         {/* End Hero Text */}
@@ -59,45 +167,51 @@ export default class Home extends React.Component {
             <section className="appointment">
                 <div className="container">
                     {/* Appointment Form */}
-                    <div className="appointment-form">
-                        <div className="row">
-                            <div className="col-lg-6 col-12">
-                                <div className="appointment-title">
-                                    <span>Appointment</span>
-                                    <h2>Book An Appointment</h2>
-                                    <p>Please feel welcome to contact our friendly reception staff with any general or medical
-                                        enquiry. Our doctors will receive or return any urgent calls.</p>
+                    <form action="#" onSubmit={this.submitted}>
+                        <div className="appointment-form">
+                            <div className="row">
+                                <div className="col-lg-6 col-12">
+                                    <div className="appointment-title">
+                                        <span>Appointment</span>
+                                        <h2>Book An Appointment</h2>
+                                        <p>Please feel welcome to contact our friendly reception staff with any general or medical
+                                            enquiry. Our doctors will receive or return any urgent calls.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-lg-3 col-md-6 col-12 p-0">
+                                    <div className="appointment-input">
+                                        <label htmlFor="city"><i className="fa-solid fa-city"></i></label>
+                                        <select className="form-select" required
+                                            onChange={(e) => this.GetDepartmentByCity(e.target.value)}>
+                                            <option value=''>Select city</option>
+                                            {this.state.city.map((item) => {
+                                                return <option value={item.city}>{item.city}</option>
+                                            })}
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="col-lg-3 col-md-6 col-12 p-0">
+                                    <div className="appointment-input">
+                                        <label htmlFor="department"><i className="fa-solid fa-book-medical"></i></label>
+                                        <select name="department" id="department"
+                                            required onChange={(e) => this.onchangeService(e.target.value)}>
+                                            <option value=''>Select department</option>
+                                            {this.state.department.map((item) => {
+                                                return <option value={item.id}>{item.title}</option>
+                                            })}
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="col-lg-3 col-md-6 col-12 custom-padding">
+                                    <div className="appointment-btn button">
+                                        <button type='submit' className="btn">Get Appointment</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="row">
-                            <div className="col-lg-3 col-md-6 col-12 p-0">
-                                <div className="appointment-input">
-                                    <label htmlFor="city"><i class="fa-solid fa-city"></i></label>
-                                    <input type="text" name="city" id="city" placeholder="Your city" />
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-md-6 col-12 p-0">
-                                <div className="appointment-input">
-                                    <label htmlFor="department"><i class="fa-solid fa-book-medical"></i></label>
-                                    <select name="department" id="department">
-                                        <option value="none" selected disabled>Department</option>
-                                        <option value="none">General Surgery</option>
-                                        <option value="none">Gastroenterology</option>
-                                        <option value="none">Nutrition &amp; Dietetics</option>
-                                        <option value="none">Cardiology</option>
-                                        <option value="none">Neurology</option>
-                                        <option value="none">Pediatric</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-md-6 col-12 custom-padding">
-                                <div className="appointment-btn button">
-                                    <button className="btn">Get Appointment</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    </form>
                     {/* End Appointment Form */}
                 </div>
             </section>
@@ -148,8 +262,14 @@ export default class Home extends React.Component {
                         </div>
                     </div>
                     <div className="row">
-                        <SpecalitiesItem title='Hair Growth Treatment' />
+                        <div className="col-lg-4 col-md-12 col-12">
+                            <div className="portfolio-sidebar">
+                                <div className="single-widget researcher-details">
+                                    <h2 className="text-center text-success">Dental Care Treatment</h2>
+                                </div>
 
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -159,3 +279,4 @@ export default class Home extends React.Component {
         );
     }
 }
+export default withCookies(Home);
