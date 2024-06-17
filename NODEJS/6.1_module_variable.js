bfc51b0d37768fac02d1f047cb2d1a5b615bc25a
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-06-17 14:49:08 +0530

    refactor: Update NODEJS/6_common&object_variable.js with common variables and database configuration

---
+++ NODEJS/6.1_module_variable.js
@@ -0,0 +1,12 @@
+const common = require("./6_common&object_variable.js");
+console.log(common.name);
+console.log(common.mobile);
+console.log(common.email);
+
+
+var db = require('./6_common&object_variable.js');
+console.log(db.database);
+console.log(db.username);
+console.log(db.password);
+console.log(db.port);
+console.log(db.name);
