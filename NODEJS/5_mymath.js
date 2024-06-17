b2e14d563962f1a88d04e622565404ad03c46b3d
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-06-17 14:48:45 +0530

    refactor: Add mymath module for basic mathematical operations

---
+++ NODEJS/5_mymath.js
@@ -0,0 +1,15 @@
+var mymath = {
+    add: function(a,b){
+        return a+b;
+    },
+    sub: function(a,b){
+        return a-b;
+    },
+    mul: function(a,b){
+        return a*b;
+    },
+    div: function(a,b){
+        return a/b;
+    }
+}
+module.exports = mymath;
