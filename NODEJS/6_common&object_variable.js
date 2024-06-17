b9f191ad3c64838ed3d3ddd927abfa9a3d49c78e
kuldeepH17 <humbalkuldeep54@gamil.com>
2024-06-17 14:49:01 +0530

    refactor: Update NODEJS/6_common&object_variable.js with common variables and database configuration

---
+++ NODEJS/6_common&object_variable.js
@@ -0,0 +1,10 @@
+module.exports.name = "THE EASYLEARN ACADEMY";
+module.exports.mobile = "9662512857";
+module.exports.email = "theeasylearn@gmail.com";
+module.exports = {
+    database:'frontend31',
+    username:'root',
+    password:'123123',
+    port:3308,
+    name:'localhost'
+}
